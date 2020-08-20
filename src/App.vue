fe1df5164f0e87789b159261e34cb149514bdfad
melaniegrigg <melanie.grigger@gmail.com>
2020-08-19 20:51:49 -0400

    add sign up and log in routes

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/ratings">Ratings</router-link>
+      <span v-if="!isLoggedIn()"><router-link to="/signup">Signup</router-link></span>|
+      <span v-if="!isLoggedIn()"><router-link to="/login">Login</router-link></span>|
+      <span v-if="isLoggedIn()"><router-link to="/logout">Logout</router-link> |</span>
     </div>
     <router-view/>
   </div>
@@ -30,3 +33,21 @@
   color: #42b983;
 }
 </style>
+<script>
+import axios from "axios";
+
+export default {
+  methods: {
+    isLoggedIn: function () {
+      if (localStorage.getItem("jwt")) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    getUserID: function () {
+      return localStorage.getItem("user_id");
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <h2>{{ message2 }}</h2>
+    <p>{{ wines }}</p>
   </div>
 </template>
 
@@ -13,14 +15,22 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Welcome to Which Wine!",
+      message2:
+        "Just select your taste preferences and we will select wine varietals that match",
       wines: [],
     };
   },
-  created: function () {},
+  created: function () {
+    this.indexWines();
+  },
   methods: {
     indexWines: function () {
-      axios;
+      console.log("indexing wines");
+      axios.get("/api/wines").then((response) => {
+        console.log(response.data);
+        this.wines = response.data;
+      });
     },
   },
 };
