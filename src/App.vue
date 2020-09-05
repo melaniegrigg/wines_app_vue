2a3280e4f5f96368ce49e7c8bfa64961a70df92d
melaniegrigg <melanie.grigger@gmail.com>
2020-09-05 16:39:08 -0400

    fix header image

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
-  <div id="app">
-    <div id="wrapper">
+<html>
+	<head>
+		<title>Which Wine? : Melanie Grigger</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
 
-      <!-- Header -->
+		<!-- Wrapper -->
+  <div id="wrapper">
+
+    <!-- Header -->
       <header id="header" class="alt">
+        <span class="logo"><img src="https://i0.wp.com/www.eatthis.com/wp-content/uploads/2019/08/wine-and-cheese-board.jpg?fit=1200%2C879&ssl=1" alt="Wine and cheese board" /></span>
+        <h1>Welcome to "Which Wine?"</h1>
+        <h2 class="logo">Simpifying wine selection</h2>
       </header>
 
-      <!-- Nav -->
+    <!-- Nav -->
       <nav id="nav">
         <ul>
           <li><a href="/" class="active">Which Wine?</a></li>
@@ -17,31 +30,50 @@
         </ul>
       </nav>
 
-       <router-view/>
+    <!-- Main -->
+      <div id="main">
 
-      <!-- Footer -->
+        <!-- Introduction -->
+          <section id="intro" class="main">
+            <div class="spotlight">
+              <div class="content">
+                <header class="major">
+                  <h2>Select your taste or smell preferences below and get your personalized wine varietal</h2>
+                </header>
+                <ul class="actions">
+                  <li>Create an account or log in to save or rate particular wines by Vintner.</li>
+                </ul>
+              </div>
+              <span class="image"><img src="https://i.pinimg.com/originals/b2/c3/76/b2c376a572acc4d1e81dffe555b7b635.jpg" alt="" /></span>
+            </div>
+          </section>
+
+      </div>
+
+    <!-- Footer -->
       <footer id="footer">
         <section>
-          <ul class="icons">
-            <li><a href="https://twitter.com/MelanieGrigger" class="icon brands fa-twitter alt"><span class="label">Twitter</span></a></li>
-            <li><a href="#" class="icon brands fa-facebook-f alt"><span class="label">Facebook</span></a></li>
-            <li><a href="#" class="icon brands fa-instagram alt"><span class="label">Instagram</span></a></li>
-            <li><a href="https://github.com/melaniegrigg" class="icon brands fa-github alt"><span class="label">GitHub</span></a></li>
+          <h2>About Which Wine</h2>
+          <p>"Which Wine?" is a web app made by Melanie Grigger for a portfolio project. It utilizes Ruby, Rails, VueJS, ActiveRecord, Restful Routes, HTML, CSS and Javascript to create a full stack, user friendly application for wine selection.</p>
+          <ul class="actions">
+            <li><a href="http://melaniegrigger.com/" class="button">Contact Me</a></li>
           </ul>
         </section>
-        <p class="copyright">&copy; Melanie Grigger: Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+        <section>
+          <h2>Like what you see? Check out my other work.</h2>
+          <ul class="icons">
+            <li><a href="#" class="icon brands fa-twitter alt"><span class="label">Twitter</span></a></li>
+            <li><a href="#" class="icon brands fa-facebook-f alt"><span class="label">Facebook</span></a></li>
+            <li><a href="#" class="icon brands fa-instagram alt"><span class="label">Instagram</span></a></li>
+            <li><a href="#" class="icon brands fa-github alt"><span class="label">GitHub</span></a></li>
+          </ul>
+        </section>
+        <p class="copyright">&copy; Melanie Grigger. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
       </footer>
 
-        <!-- </div>
-      <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/ratings">Ratings</router-link> |
-        <span v-if="!isLoggedIn()"><router-link to="/signup">Signup</router-link></span>|
-        <span v-if="!isLoggedIn()"><router-link to="/login">Login</router-link></span>|
-        <span v-if="isLoggedIn()"><router-link to="/logout">Logout</router-link> </span>
-      </div> -->
-    </div>
   </div>
+	</body>
+</html>
 </template>
 
 <style>
